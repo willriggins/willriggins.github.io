d1580e2d347e5f10ff12fc4c786c75a9742b24dc
willriggins <williamriggins2013@gmail.com>
2016-08-15 00:43:54 -0400

    nav hiding properly

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,12 +1,3 @@
-//jQuery to collapse the navbar on scroll
-// $(window).scroll(function() {
-//     if ($(".navbar").offset().top > 50) {
-//         $(".navbar-fixed-top").addClass("top-nav-collapse");
-//     } else {
-//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-//     }
-// });
-
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
@@ -17,42 +8,17 @@ $(function() {
         event.preventDefault();
     });
 });
-// working scroll fx
-// (function ($) {
-//   $(document).ready(function(){
-//
-// 	// hide .navbar first
-// 	$(".navbar").hide();
-//   $(".navbar-fixed-top").addClass("top-nav-collapse");
-//
-// 	// fade in .navbar
-// 	$(function () {
-// 		$(window).scroll(function () {
-//             // set distance user needs to scroll before we fadeIn navbar
-//       if ($(".navbar").offset().top > 300) {
-// 				$('.navbar').fadeIn();
-// 			} else {
-// 				$('.navbar').fadeOut();
-// 			}
-// 		});
-//
-//
-// 	});
-//
-// });
-//   }(jQuery));
 
-//scrolling functions.. based on sections
+// hiding nav bar while scrolling
 
-$(window).scroll(function(){
+$(function () {
+	$(window).scroll(function () {
 
-  if ($(this).scrollTop() <= $('#intro').offset().top - 25) {
-      $("nav").fadeOut(500);
-  }
-
-// intro section
- if ($(this).scrollTop() >= $('#intro').offset().top - 25) {
-     $("nav").fadeIn(500);
- }
-
+    if ($(this).scrollTop() <= $('#about').offset().top) {
+      $('.navbar').fadeOut(500);
+    }
+    if ($(this).scrollTop() > $('#about').offset().top) {
+			$('.navbar').fadeIn(500);
+		}
+	});
 });
